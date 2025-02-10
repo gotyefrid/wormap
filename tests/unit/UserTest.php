15ec9dcc7288ee5b18d7790281593ca5edf354d9
Vladislav <wladoxa@gmail.com>
2025-02-10 09:33:37 +0300

    Тесты

---
+++ tests/unit/UserTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace WorMap\tests\unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    public function testEx()
+    {
+        static::assertTrue(true);
+    }
+}
