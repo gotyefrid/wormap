f63fda032ac9d41fddae0c04b784c1e4699a2214
Vladislav <wladoxa@gmail.com>
2025-02-11 21:52:02 +0300

    Установка псалма

---
+++ tests/unit/UserServiceTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UserServiceTest extends TestCase
+{
+    public function testEx()
+    {
+        static::assertTrue(true);
+    }
+}
